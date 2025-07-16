c3a35cef09e3be9bbd87b86622fa9da57645afbb
rameel ahmed <rameelahmed786@gmail.com>
2025-07-16 10:30:24 -0400

    updated

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ df=pd.read_csv("data/topics.csv")
 pdf=FPDF(orientation="P",unit="mm",format="A4")
 
 for index,row in df.iterrows():
-    print(index,row)
-    pdf.add_page()
+    for i in range(row["Pages"]):
+        pdf.add_page()
 
-    pdf.set_font(family="Times",style="B",size=24)
-    pdf.set_text_color(100,100,100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,21,200,21)
+        pdf.set_font(family="Times",style="B",size=24)
+        pdf.set_text_color(100,100,100)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+        pdf.line(10,21,200,21)
 
 pdf.output("output.pdf")
